131eb8a88d139f683e23adbbc0d7f0c4f71eac89
Iranildo Junior <156467210+Iramn123@users.noreply.github.com>
2024-02-01 17:30:20 -0300

    Add files via upload

---
+++ lista dinamica.py
@@ -0,0 +1,60 @@
+itens_quantity = int(input("Quantos itens voce deseja comprar? "))
+
+
+itens_list = []
+
+for item in range(itens_quantity):
+    itens_wanted = input(f"item {item+1}: ")
+    itens_list.append(itens_wanted)
+   
+
+
+def enumerate_lista(list):
+    print("Assim ficou sua lista...")
+
+    for number, item in enumerate(list):
+        print(F"{number} - {item}")
+   
+
+
+while True:
+    enumerate_lista(itens_list)
+    question1 = input('Deseja adicionar/remover algum item? Caso não queira, insira "Nao"').lower()
+    
+    if "nao" in question1:
+        question2 = input('Deseja encerrar o programa? (S/N)').lower()
+
+        if question2 == "s":
+            break
+
+    if question1 == 'remover':
+        iten_remover = input("Que item voce deseja remover? insira nome/numero: ")
+            
+        if iten_remover.isdigit():
+            index_remover = int(iten_remover) - 1
+            try:
+                itens_list.pop(index_remover)
+            except:
+                print("Numero nao esta presente na lista, verifique se esta certo!")
+                continue
+
+
+        elif iten_remover in itens_list:
+            try:
+                itens_list.remove(iten_remover)
+            except:
+                print("O item nao esta presente na lista, verifique se esta certo!")
+                continue
+        
+
+    elif question1 == 'adicionar':
+        iten_adder = input("Que item voce deseja adicionar? ")
+        if iten_adder.isdigit():
+            print("Não é permitido adicionar numeros")
+            break
+        itens_list.append(iten_adder)
+            
+        
+    else:
+        print('Digite apenas "adicionar/remover"')
+        continue
