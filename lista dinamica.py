fae88ac1d26b1d33536f1cee0c4c980e0bd9a963
Iranildo Junior <iranildojunior777@gmail.com>
2024-02-02 01:21:53 -0300

    A very changes in the code, specially a wrong words

--- lista dinamica.py
+++ lista dinamica.py
@@ -1,4 +1,4 @@
-itens_quantity = int(input("Quantos itens voce deseja comprar? "))
+itens_quantity = int(input("Quantos itens você deseja comprar? "))
 
 
 itens_list = []
@@ -12,7 +12,7 @@ for item in range(itens_quantity):
         else: break
 
     itens_list.append(itens_wanted)
-   
+
 
 
 def enumerate_lista(list):
@@ -20,48 +20,48 @@ def enumerate_lista(list):
 
     for number, item in enumerate(list, 1):
         print(F"{number} - {item}")
-   
+
 
 
 while True:
     enumerate_lista(itens_list)
-    question1 = input('Deseja adicionar/remover algum item? Caso não queira, insira "Nao" ').lower()
+    question1 = input('Deseja adicionar/remover algum item? Caso não queira, insira "Não" ').lower()
     
-    if "nao" in question1: break
-        
+    if "n" in question1: break
+
 
 
     if question1 == 'remover':
-        iten_remover = input("Que item voce deseja remover? insira nome/numero: ")
+        item_remover = input("Que item você deseja remover? insira nome/número: ")
             
-        if iten_remover.isdigit():
-            index_remover = int(iten_remover) - 1
+        if item_remover.isdigit():
+            index_remover = int(item_remover) - 1
             try:
                 itens_list.pop(index_remover)
             except:
-                print("Numero nao esta presente na lista, verifique se esta certo!")
+                print("Número não está presente na lista, verifique se está certo!")
                 continue
 
 
-        elif iten_remover in itens_list:
+        elif item_remover in itens_list:
             try:
-                itens_list.remove(iten_remover)
+                itens_list.remove(item_remover)
             except:
-                print("O item nao esta presente na lista, verifique se esta certo!")
+                print("O item não está presente na lista, verifique se está certo!")
                 continue
-        
+
 
 
     elif question1 == 'adicionar':
-        iten_adder = input("Que item voce deseja adicionar? ")
+        item_adder = input("Que item você deseja adicionar? ")
 
-        if iten_adder.isdigit():
-            print("Não é permitido adicionar numeros")
-            break
+        if item_adder.isdigit():
+            print("Não é permitido adicionar números")
+            continue
 
-        itens_list.append(iten_adder)
-            
-    
+        itens_list.append(item_adder)
+
+
 
     else:
         print('Digite apenas "adicionar/remover"')
